1f6e87d7a9d12d15773951ab10f25c28c305830a
avery.radmacher <avery.radmacher@gmail.com>
2019-10-07 18:24:57 -0400

    Port Cipher initializer

--- src/delfin/unittester.go
+++ src/delfin/unittester.go
@@ -8,5 +8,9 @@ import (
 // entry point for unit tester
 func main() {
 	fmt.Println("Unit tester")
-	dcipher.Test()
+	c := dcipher.NewCipher([16]byte{255})
+	c.InternalInfo()
+	for i := 0; i < 8; i++ {
+		fmt.Println(c.GetByte())
+	}
 }
