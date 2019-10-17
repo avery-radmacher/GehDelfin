c3740d384596b339e6fd331e5c8e7454a3a1ae94
avery.radmacher <avery.radmacher@gmail.com>
2019-10-16 20:24:40 -0400

    Rewrite unit tester

--- src/delfin/unittester.go
+++ src/delfin/unittester.go
@@ -3,29 +3,39 @@ package main
 import (
 	dcipher "cipher"
 	"fmt"
+	"images"
 	"os"
 )
 
 // entry point for unit tester
 func main() {
 	fmt.Println("Unit tester")
-	password := "delfin"
-	if len(os.Args) > 1 {
-		password = os.Args[1]
+	if len(os.Args) == 1 {
+		fmt.Println("no test specified")
+		return
 	}
-	c := dcipher.NewCipherv2(password)
-	m := make(map[byte]uint16)
-	b := make([]byte, 256*1024)
-	c.Crypt(b)
-	for _, val := range b {
-		m[val]++
-	}
-	fmt.Println()
-	for i := byte(0); i < 16; i++ {
-		for j := byte(0); j < 16; j++ {
-			fmt.Printf("%2X: %4d | ", i*16+j, m[byte(i*16+j)])
+	switch os.Args[1] {
+	case "cipher":
+		password := "delfin"
+		if len(os.Args) > 2 {
+			password = os.Args[2]
+		}
+		c := dcipher.NewCipherv2(password)
+		m := make(map[byte]uint16)
+		b := make([]byte, 256*1024)
+		c.Crypt(b)
+		for _, val := range b {
+			m[val]++
 		}
 		fmt.Println()
+		for i := byte(0); i < 16; i++ {
+			for j := byte(0); j < 16; j++ {
+				fmt.Printf("%2X: %4d | ", i*16+j, m[byte(i*16+j)])
+			}
+			fmt.Println()
+		}
+		fmt.Println(c)
+	case "image":
+		images.Test()
 	}
-	fmt.Println(c)
 }
